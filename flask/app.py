54f72424c8a5e07701c23f9478ae0457cf2b957d
PingoFang <110073165+PingoFang@users.noreply.github.com>
2022-07-29 10:44:06 +0800

    Add files via upload

--- flask/app.py
+++ flask/app.py
@@ -13,6 +13,34 @@ from flask import render_template
 
 app = Flask(__name__)
 
+#test watchlist.html
+
+user= {
+       'username': 'Grey Li ',
+       'bio':'A boy who loves movies and music .' ,
+          }
+
+
+movies= [
+    { 'name':' My Neighbor Totaro ','year ' : ' 1988 ' },
+    { 'name' : 'Three Colours 七rilogy ' ,' year ' : ' 1993 ' },
+    { 'name' : ' Forrest Gump ', ' year ':' 1994 ' },
+    { 'name' : 'Perfect Blue ',' year ':' 1997 ' },
+    { 'name': 'The Matrix','year ':'1999 ' },
+    { 'name': 'Memento ' , ' year ' :'2000 ' },
+    { 'name' :' The Bucket lis 七',' year ': ' 2007 ' } ,
+    { 'name':' Black Swan ',' year ' : ' 2010 ' },
+    { 'name':' Gone Girl ', ' year ' :'2014 ' },
+    { 'name':' CoCo ',' year ':' 2017 ' } ,
+]
+ 
+@app.route ('/watchlist')
+def watchlist () :
+    return render_template( 'watchlist.html', user=user , movies=movies )
+    
+
+
+
 @app.route('/')
 def index():
     return 'Index Page'
